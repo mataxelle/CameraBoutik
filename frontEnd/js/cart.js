6cc2b79f50d92cd6c6c9e7e28f138647ad0c3f11
axelle <kadance9720@gmail.com>
2021-05-25 15:51:18 +0200

    Total panier ok

--- frontEnd/js/cart.js
+++ frontEnd/js/cart.js
@@ -1,43 +1,60 @@
 //récup du localStorage
 let localStorageProduct = JSON.parse(localStorage.getItem('product'));
 
-const productName = document.querySelector('.name');
-const lenses = document.querySelector('.lenses');
-const quantity = document.querySelector('.quantity');
-const price = document.querySelector('.price');
 
 if (localStorageProduct < 1) {
-    
+
     document.querySelector('.cart_empty_msg').textContent = "Votre panier est vide !";
 
     document.querySelector('.cart_full_msg').style.display = 'none';
 
 } else {
-    
+
     document.querySelector('.cart_empty_msg').style.display = 'none';
 
-    let localTableProduct = [];
+    let tableProduct = [];
+    let total = 0;
 
     for (let i = 0; i < localStorageProduct.length; i++) {
         const oneProduct = localStorageProduct[i];
 
-        localTableProduct = localTableProduct + `
+        tableProduct = tableProduct + `
         <div class="row text-center border cart_row my-2">
-                        <div class="col">nom
+                        <div class="col">Nom
                             <div class="name">${oneProduct.name}</div>
                         </div>
-                        <div class="col">lentilles
+                        <div class="col">Lentilles
                             <div class="lenses">${oneProduct.lenses}</div>
                         </div>
-                        <div class="col">qt
+                        <div class="col">Quantité
                             <div class="quantity">${oneProduct.quantity}</div>
                         </div>
-                        <div class="col">prix
+                        <div class="col">Prix
                             <div class="price">${oneProduct.price}</div>
                         </div>
-                    </div>
+                        <div class="col">
+                            <button class="supBtn mt-2">X</button>
+                        </div>
+        </div>
         `;
 
-        document.querySelector('.cart_full_msg').innerHTML = localTableProduct;
+        let totalQuantity = oneProduct.quantity++;
+
+        let subTotal = oneProduct.price * totalQuantity;
+
+        total = total + subTotal;
+        console.log(total);
     }
+
+    document.querySelector('.cart_full_msg').innerHTML = tableProduct;
+
+    let totalDiv = ` <div class="row">
+                            <div class="col col-3 offset-9">Total :
+                                <span class="total">${total}</span>
+                            </div>
+                        </div>
+    `;
+
+    //document.querySelector('.cart_full_msg').innerHTML = tableProduct;
+    document.querySelector('.cart_full_msg').innerHTML += totalDiv;  // += sinon remplace le premier innerHTML
 }
